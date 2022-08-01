826f1c27ad87c0da4d4485d77069e86525080225
Topcoder415 <alenschmitt415@gmail.com>
2022-08-01 13:50:56 +0530

    changes for heroku deployment

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Client'));
-
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Client/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+
+'/dist/Client/index.html'));});
 app.listen(process.env.PORT || 8080);
