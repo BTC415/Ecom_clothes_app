8d64f7e18d9f895916c9f20ae5fedee824ae02ac
Topcoder415 <alenschmitt415@gmail.com>
2022-08-01 14:18:38 +0530

    heroku changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/Client'));
+app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/Client/index.html'));});
+'/dist/client/index.html'));});
 app.listen(process.env.PORT || 8080);
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,80 +1,81 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  env: any = environment;
   constructor(private http: HttpClient) {}
 
   SignUp(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/signup', data);
+    return this.http.post(this.env.apiUrl + 'singup', data);
   }
   UserLogin(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/login', data);
+    return this.http.post(this.env.apiUrl + 'login', data);
   }
   //send email
   SendEmail(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/sendmail', { email: data });
+    return this.http.post(this.env.apiUrl + 'sendmail', { email: data });
   }
 
   //forgot password
   ForgotPassword(data: any): Observable<any> {
-    return this.http.put('http://localhost:3000/forgotpwd', data);
+    return this.http.put(this.env.apiUrl + 'forgotpwd', data);
   }
 
   //edit profile
   EditProfile(data: any): Observable<any> {
-    return this.http.put('http://localhost:3000/editprofile', data);
+    return this.http.put(this.env.apiUrl + 'editprofile', data);
   }
 
   //get all products
   getProducts(category?: any): Observable<any> {
     if (category == undefined) {
-      return this.http.get('http://localhost:3000/products');
+      return this.http.get(this.env.apiUrl + 'products');
     } else {
-      return this.http.get('http://localhost:3000/products', {
+      return this.http.get(this.env.apiUrl + 'products', {
         params: { category: category },
       });
     }
   }
   getCollectionProducts(collection?: any): Observable<any> {
-    return this.http.get('http://localhost:3000/products', {
+    return this.http.get(this.env.apiUrl + 'products', {
       params: { collection: collection },
     });
   }
   getNewInProducts(newin?: any): Observable<any> {
-    return this.http.get('http://localhost:3000/products', {
+    return this.http.get(this.env.apiUrl + 'products', {
       params: { newin: newin },
     });
   }
 
   //get one product
   GetOneProduct(_id: any): Observable<any> {
-    return this.http.get('http://localhost:3000/product/' + _id);
+    return this.http.get(this.env.apiUrl + 'product/' + _id);
   }
 
   //change password
   ChagePassword(data: any): Observable<any> {
-    return this.http.put('http://localhost:3000/changepassword', data);
+    return this.http.put(this.env.apiUrl + 'changepassword', data);
   }
 
   //add wishlist
   addWishList(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/addwishlist', data);
+    return this.http.post(this.env.apiUrl + 'addwishlist', data);
   }
 
   //display wishlist
   getWishlistData(): Observable<any> {
-    return this.http.get('http://localhost:3000/wishlistdata', {
+    return this.http.get(this.env.apiUrl + 'wishlistdata', {
       headers: this.getHeaders(),
     });
   }
 
   //delete wishlist
   deleteWishList(data: any): Observable<any> {
-    return this.http.delete('http://localhost:3000/deletewishlist', {
+    return this.http.delete(this.env.apiUrl + 'deletewishlist', {
       headers: this.getHeaders(),
       body: data,
     });
@@ -82,26 +83,26 @@ export class ApiService {
 
   //add cart
   addCart(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/addcart', data);
+    return this.http.post(this.env.apiUrl + 'addcart', data);
   }
 
   //display cart data
   getCartData(): Observable<any> {
-    return this.http.get('http://localhost:3000/getcart', {
+    return this.http.get(this.env.apiUrl + 'getcart', {
       headers: this.getHeaders(),
     });
   }
 
   //delete cart product
   deleteCartData(data: any): Observable<any> {
-    return this.http.delete('http://localhost:3000/deletecart', {
+    return this.http.delete(this.env.apiUrl + 'deletecart', {
       headers: this.getHeaders(),
       body: data,
     });
   }
 
   Userloggedin(): Observable<any> {
-    return this.http.get('http://localhost:3000/userloggedin', {
+    return this.http.get(this.env.apiUrl + 'userloggedin', {
       headers: this.getHeaders(),
     });
   }
